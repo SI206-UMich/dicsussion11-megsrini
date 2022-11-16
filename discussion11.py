aa6d343e735a997fa9ceb23d26407d511a13cace
Meghana Srinivasa <megsrini@umich.edu>
2022-11-15 19:27:13 -0500

    Finished task 1.

--- discussion11.py
+++ discussion11.py
@@ -39,12 +39,15 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
+    cur.execute("DROP TABLE IF EXISTS Patients")
+    cur.execute("CREATE TABLE Patients (pet_id INTEGER, name STRING, species_id NUMBER, age INTEGER, cuteness INTEGER, aggressiveness NUMBER)")
+    
 
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
+    cur.execute("INSERT INTO Patients (pet_id,name,species_id,age,cuteness,aggressiveness) VALUES (?,?,?,?,?,?)",(0,"Fluffle",0,3,90,100))
+    conn.commit()
     
 
 # TASK 2
@@ -59,13 +62,51 @@ def add_pets_from_json(filename, cur, conn):
     json_data = json.loads(file_data)
 
     # THE REST IS UP TO YOU
-    pass
+    
+    for i in range(len(json_data)):
+        if json_data[i]["species"] == "Rabbit":
+            json_data[i]["species"] = 0
+        elif json_data[i]["species"] == "Dog":
+            json_data[i]["species"] = 1
+        elif json_data[i]["species"] == "Cat":
+            json_data[i]["species"] = 2
+        elif json_data[i]["species"] == "Boa Constrictor":
+            json_data[i]["species"] = 3
+        elif json_data[i]["species"] == "Chinchilla":
+            json_data[i]["species"] = 4
+        elif json_data[i]["species"] == "Hamster":
+            json_data[i]["species"] = 5
+        elif json_data[i]["species"] == "Cobra":
+            json_data[i]["species"] = 6
+        elif json_data[i]["species"] == "Parrot":
+            json_data[i]["species"] = 7
+        elif json_data[i]["species"] == "Shark":
+            json_data[i]["species"] = 8
+        elif json_data[i]["species"] == "Goldfish":
+            json_data[i]["species"] = 9
+        elif json_data[i]["species"] == "Gerbil":
+            json_data[i]["species"] = 10
+        elif json_data[i]["species"] == "Llama":
+            json_data[i]["species"] = 11
+        elif json_data[i]["species"] == "Hare":
+            json_data[i]["species"] = 12
+
+        cur.execute("INSERT INTO Patients (pet_id,name,species_id,age,cuteness,aggressiveness) VALUES (?,?,?,?,?,?)",(i+1,json_data[i]["name"],json_data[i]["species"],json_data[i]["age"],json_data[i]["cuteness"],json_data[i]["aggressiveness"]))
+    conn.commit()
+
+
 
 
 # TASK 3
 # CODE TO OUTPUT NON-AGGRESSIVE PETS
 def non_aggressive_pets(aggressiveness, cur, conn):
-    pass
+    cur.execute("SELECT name FROM Patients WHERE aggressiveness<="+ str(aggressiveness))
+    conn.commit()
+    result = cur.fetchall()
+    return result
+
+
+
 
 
 
